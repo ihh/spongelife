5f40de18110d8a1e6bfc00fa3e89433c32d05464
Ian Holmes <ihh@berkeley.edu>
2018-12-15 17:46:30 -0800

    web demo

--- Makefile
+++ Makefile
@@ -4,15 +4,20 @@ BRACERY = $(HOME)/bracery/bin/bracery
 
 # Data files
 DATADIR = data
+WEBDIR = web
+
 SYMDIR = $(DATADIR)/symbols
 TMPLDIR = $(DATADIR)/templates
 MAIN_TEMPLATE = $(TMPLDIR)/main.bracery
-ALL_TEMPLATES = $(TMPLDIR)/markov.bracery
 
-SYMBOLS = $(wildcard $(SYMDIR)/*.bracery) $(wildcard $(SYMDIR)/*.json)
+ALL_TEMPLATES = $(WEBDIR)/markov.bracery
+ALL_SYMBOLS = $(WEBDIR)/symbols.bracery
+
+SYMBOLS_BRACERY = $(wildcard $(SYMDIR)/*.bracery)
+SYMBOLS_JSON = $(wildcard $(SYMDIR)/*.json)
 TEMPLATES = $(filter-out $(ALL_TEMPLATES),$(filter-out $(MAIN_TEMPLATE),$(wildcard $(TMPLDIR)/*.bracery)))
 
-DEFS = $(addprefix -d ,$(SYMBOLS))
+DEFS = -d $(ALL_SYMBOLS)
 MARKOV = -m $(ALL_TEMPLATES)
 QUIZ = -q $(ALL_TEMPLATES)
 
@@ -24,26 +29,26 @@ endif
 all: demo
 
 # Run modes
-demo: templates
+demo: compendium
 	$(BRACERY) $(RNDSEED) $(DEFS) $(MARKOV)
 
-tags: templates
+tags: compendium
 	$(BRACERY) $(RNDSEED) $(DEFS) $(MARKOV) -v1
 
-vars: templates
+vars: compendium
 	$(BRACERY) $(RNDSEED) $(DEFS) $(MARKOV) -v2
 
-game: templates
+game: compendium
 	$(BRACERY) $(RNDSEED) $(DEFS) $(QUIZ)
 
-debug: templates
+debug: compendium
 	$(BRACERY) $(RNDSEED) $(DEFS) $(QUIZ) -v2
 
 # Intermediate files
-templates: $(ALL_TEMPLATES)
+compendium: $(ALL_TEMPLATES) $(ALL_SYMBOLS)
 
 clean:
-	rm $(ALL_TEMPLATES)
+	rm $(ALL_TEMPLATES) $(ALL_SYMBOLS)
 
 $(ALL_TEMPLATES): $(MAIN_TEMPLATE) $(TEMPLATES)
 	@cat $(MAIN_TEMPLATE) >$@
@@ -52,5 +57,21 @@ $(ALL_TEMPLATES): $(MAIN_TEMPLATE) $(TEMPLATES)
 	 cat $$FILE >>$@; \
 	 echo \#\# RESET >>$@; \
 	 done
+
+$(ALL_SYMBOLS): $(SYMBOLS_BRACERY) $(SYMBOLS_JSON)
+	@$(BRACERY) $(addprefix -d ,$(SYMBOLS_JSON)) -O $@
+	@for FILE in $(SYMBOLS_BRACERY) ; do \
+	 echo >>$@; \
+	 cat $$FILE >>$@; \
+	 echo \#\# RESET >>$@; \
+	 done
+
+# Web
+webstart:
+	npm install
+	$(MAKE) web/bracery.js
+
+web/bracery.js: node_modules/bracery/web/bracery.js
+	cp $< $@
 
 .SECONDARY:
---
+++ web/markov.js
@@ -0,0 +1,113 @@
+function messageDiv (message, config) {
+  config = config || {}
+  var e = document.createElement('div')
+  e.innerHTML = ((config.title
+                  ? (message.template.title
+                     ? (('[' + message.template.title + ']') + ' ')
+                     : '')
+                  : '')
+                 + (message.template.author
+                    ? (message.template.author + ': ')
+                    : '')
+                 +  message.expansion.text)
+  if (config.style)
+    e.setAttribute('style',config.style)
+  return e
+}
+
+var textdefsElement = document.getElementById('textdefs')
+var tempdefsElement = document.getElementById('tempdefs')
+var threadElement = document.getElementById('thread')
+var resetElement = document.getElementById('reset')
+
+function update() {
+  try {
+    var defs = {}
+    if (textdefsElement.value.match(/\S/)) {
+      try {
+        defs = JSON.parse (textdefsElement.value)
+      } catch (e) {
+        defs = bracery.ParseTree.parseTextDefs (textdefsElement.value)
+      }
+    }
+
+    var templates = []
+    if (tempdefsElement.value.match(/\S/)) {
+      try {
+        templates = JSON.parse (tempdefsElement.value)
+      } catch (e) {
+        templates = bracery.Template.parseTemplateDefs (tempdefsElement.value)
+      }
+    }
+
+    var b = new bracery.Bracery (defs)
+    var markovConfig = {
+      bracery: b,
+      templates: templates,
+      vars: {},
+      accept: function (message, thread, resolve) {
+        threadElement.innerHTML = ""
+        if (thread)
+          thread.forEach (function (threadMessage) {
+            threadElement.appendChild (messageDiv (threadMessage, { style: 'background-color: #ddd;' }))
+          })
+        threadElement.appendChild (messageDiv (message, { title: true, style: 'font-style: italic; background-color: #bbb;' }))
+        var acceptElement = document.createElement('a')
+        var rejectElement = document.createElement('a')
+        var span = document.createElement('span')
+        acceptElement.innerText = 'Accept'
+        rejectElement.innerText = 'Reject'
+        span.innerText = ' / '
+        acceptElement.setAttribute ('href', '#')
+        rejectElement.setAttribute ('href', '#')
+        threadElement.appendChild (acceptElement)
+        threadElement.appendChild (span)
+        threadElement.appendChild (rejectElement)
+        acceptElement.addEventListener ('click', function(e) {
+          e.preventDefault()
+          resolve (true)
+        })
+        rejectElement.addEventListener ('click', function(e) {
+          e.preventDefault()
+          resolve (false)
+        })
+      }
+    }
+
+    currentAccept = currentReject = null
+    bracery.Template.promiseMessageList (markovConfig)
+      .then (function (thread) {
+        threadElement.innerHTML = ""
+        if (thread)
+          thread.forEach (function (threadMessage) {
+            threadElement.appendChild (messageDiv (threadMessage, { title: true }))
+          })
+      })
+    
+  } catch (e) {
+    threadElement.innerText = e
+  }
+}
+textdefsElement.addEventListener ('keyup', update)
+tempdefsElement.addEventListener ('keyup', update)
+resetElement.addEventListener ('click', update)
+
+var symbolsUrl = window.location.href + "symbols.bracery"
+var markovUrl = window.location.href + "markov.bracery"
+
+var defsReq = new XMLHttpRequest();
+defsReq.addEventListener("load", function() {
+  textdefsElement.value = this.responseText
+
+  var tempReq = new XMLHttpRequest();
+  tempReq.addEventListener("load", function() {
+    tempdefsElement.value = this.responseText
+    update()
+  })
+  console.warn ('loading ' + markovUrl)
+  tempReq.open("GET", markovUrl);
+  tempReq.send()
+});
+console.warn ('loading ' + symbolsUrl)
+defsReq.open("GET", symbolsUrl);
+defsReq.send()
